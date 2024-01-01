088a797841b43a3d4f5ca48dbb3212ce13949666
Philipp Otto <philipp.4096@gmail.com>
2024-01-01 17:43:01 +0100

    use context

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import _ from "lodash";
 import { produce } from "immer";
+import React from "react";
 
 const columnNames = "abcdefghijklmno";
 
@@ -46,8 +47,9 @@ type GameState = {
     // - if it's empty
     normal: boolean;
     colors: Array<Color>;
-    star: false;
-    empty: false;
+    star: boolean;
+    empty: boolean;
+    tickable: boolean;
     inColumns: Array<string>;
   };
 };
@@ -80,51 +82,66 @@ const initialGameState: GameState = {
     normal: true,
     star: false,
     empty: false,
+    tickable: true,
     colors: [],
     inColumns: [],
   },
 };
 
+export const GameStateWithSetterContext = React.createContext({
+  gameState: initialGameState,
+  setGameState: (_callback: (state: GameState) => void) => {},
+});
+
 function getField(colIdx: number, rowIdx: number, gameState: GameState) {
   const encoded = gameLayout[colIdx][rowIdx];
   return {
-    color: letterToColorName[encoded.toLowerCase() as any],
+    color: letterToColorName[encoded.toLowerCase() as any] as Color,
     hasStar: encoded.toLowerCase() !== encoded,
     value: gameState.grid[colIdx][rowIdx],
   };
 }
 
-function Cell({
-  gameState,
-  colIdx,
-  rowIdx,
-  setGameState,
-}: {
-  colIdx: number;
-  rowIdx: number;
-  gameState: GameState;
-  setGameState: (cb: (s: GameState) => void) => void;
-}) {
+function Cell({ colIdx, rowIdx }: { colIdx: number; rowIdx: number }) {
+  const { gameState, setGameState } = React.useContext(GameStateWithSetterContext);
+
   const { color, hasStar, value } = getField(colIdx, rowIdx, gameState);
   const onClick = () => {
     setGameState((old: GameState): void => {
       old.grid[colIdx][rowIdx] = old.grid[colIdx][rowIdx] === 1 ? 0 : 1;
     });
   };
+
   return (
-    <td onClick={onClick} className={`cell ${color}`}>
+    <td
+      onClick={onClick}
+      className={`cell ${color}`}
+      style={{
+        opacity: getOpacityForCell(gameState, rowIdx, colIdx),
+      }}
+    >
       {value ? "✗" : hasStar ? "★" : ""}
     </td>
   );
 }
 
+function getOpacityForCell(gameState: GameState, rowIdx: number, colIdx: number) {
+  if (gameState.highlight.tickable) {
+    if (colIdx !== 7) {
+      return 0.5;
+    }
+    return 1;
+  }
+  return 1;
+}
+
+function getOpacityForColor(gameState: GameState, color: Color) {
+  const highlightColors = gameState.highlight.colors;
+  return highlightColors.length === 0 || highlightColors.includes(color) ? 1 : 0.25;
+}
+
 function Grid() {
-  const [gameState, _setGameState] = useState(initialGameState);
-  const setGameState = (callback: (state: GameState) => void) => {
-    _setGameState((oldValue: GameState) => {
-      return produce(oldValue, callback);
-    });
-  };
+  // const { gameState, setGameState } = React.useContext(GameStateWithSetterContext);
 
   const rows = [];
 
@@ -138,15 +155,7 @@ function Grid() {
   for (const rowIdx of _.range(0, gameLayout[0].length)) {
     const currentRow = [];
     for (const colIdx of _.range(0, gameLayout.length)) {
-      currentRow.push(
-        <Cell
-          key={colIdx}
-          gameState={gameState}
-          setGameState={setGameState}
-          colIdx={colIdx}
-          rowIdx={rowIdx}
-        />,
-      );
+      currentRow.push(<Cell key={colIdx} colIdx={colIdx} rowIdx={rowIdx} />);
     }
     rows.push(<tr key={rowIdx}>{currentRow}</tr>);
   }
@@ -163,26 +172,51 @@ function Grid() {
 }
 
 function Toolbar() {
-  const colors = Object.values(letterToColorName);
+  const { gameState, setGameState } = React.useContext(GameStateWithSetterContext);
+  const colors = Object.values(letterToColorName) as Color[];
+
+  const handleColorClick = (color: Color) => {
+    setGameState((oldState) => {
+      if (oldState.highlight.colors.includes(color)) {
+        oldState.highlight.colors = oldState.highlight.colors.filter((c) => color != c);
+      } else {
+        oldState.highlight.colors.push(color);
+      }
+    });
+  };
+
   return (
     <div style={{ display: "flex" }}>
-      {colors.map((color) => (
-        <div className={`cell ${color}`} />
+      {colors.map((color: Color) => (
+        <div
+          className={`cell ${color}`}
+          style={{
+            opacity: getOpacityForColor(gameState, color),
+          }}
+          onClick={() => handleColorClick(color)}
+        />
       ))}
     </div>
   );
 }
 
 function App() {
+  const [gameState, _setGameState] = useState(initialGameState);
+  const setGameState = (callback: (state: GameState) => void) => {
+    _setGameState((oldValue: GameState) => {
+      return produce(oldValue, callback);
+    });
+  };
+
   return (
-    <>
+    <GameStateWithSetterContext.Provider value={{ gameState, setGameState }}>
       <div className="card">
         <Toolbar />
       </div>
       <div className="card">
         <Grid />
       </div>
-    </>
+    </GameStateWithSetterContext.Provider>
   );
 }
 
